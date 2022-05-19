4e17ccd9b03115e892cf6ef93eef4df0597f2efa
hardcorebuddhist <im.energo@gmail.com>
2022-05-19 11:41:15 +0200

    all spaces + spaceById endpoints

--- routers/spaceRouter.js
+++ routers/spaceRouter.js
@@ -1,5 +1,5 @@
 const { Router } = require("express");
-
+const auth = require("../auth/middleware");
 const Space = require("../models").space;
 const Story = require("../models").story;
 
@@ -12,6 +12,27 @@ router.get("/", async (req, res, next) => {
     console.log(e);
     next(e);
   }
+});
+
+// GET space with stories by id.
+router.get("/:id", async (req, res) => {
+  const { id } = req.params;
+
+  console.log(id);
+  if (isNaN(parseInt(id))) {
+    return res.status(400).send({ message: "Space id is not a number" });
+  }
+
+  const space = await Space.findByPk(id, {
+    include: [Story],
+    order: [[Story, "createdAt", "DESC"]],
+  });
+
+  if (space === null) {
+    return res.status(404).send({ message: "Space not found" });
+  }
+
+  res.status(200).send({ message: "ok", space });
 });
 
 module.exports = router;
